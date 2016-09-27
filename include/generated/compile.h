1b6cc3461c23244f575e37aafdd5d486964635e5
LG Electronics <opensource@lge.com>
2016-09-27 04:19:30 +0000

    LGH345KT_Android_Lollipop_v10j

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Jul 22 14:04:25 KST 2016"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Sep 25 17:27:09 KST 2016"
 #define LINUX_COMPILE_BY "jaehun12.kim"
 #define LINUX_COMPILE_HOST "LGEARND13B15"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.49-ge30790f"
+#define UTS_RELEASE "3.10.49-g432abe1"
