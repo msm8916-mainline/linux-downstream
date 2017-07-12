b2166d0060e402b2b1d39678930d48570772a464
LG Electronics <opensource@lge.com>
2017-07-12 03:58:50 +0000

    LGH345_Android_Lollipop_V10p

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Tue May 9 11:52:20 IST 2017"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Jul 12 09:21:09 IST 2017"
 #define LINUX_COMPILE_BY "atosh.v"
 #define LINUX_COMPILE_HOST "si-rd10-bld43"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
